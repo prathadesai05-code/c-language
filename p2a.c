8502da94ed9a44c808034ef485ba2d7574c15f97
prathadesai05-code <prathadesai05@gmail.com>
2026-08-14 08:07:16 +0530

    Add files via upload

---
+++ p2a.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+	int num1,num2;
+	int sum,difference,product,quotient,remainder;
+	
+	printf("enter1");
+	scanf("%d",&num1);
+	
+	printf("enter2");
+	scanf("%d",&num2);
+	
+	sum=num1+num2;
+	difference=num1-num2;
+	product=num1*num2;
+	quotient=num1/num2;
+	remainder=num1%num2;
+	
+	printf("\naddition=%d",sum);
+	printf("\nsubtraction=%d",difference);
+	printf("\nmultiplication=%d",product);
+	printf("\ndivision=%d",quotient);
+	printf("\nmodulus=%d",remainder);
+	
+	return 0;
+	
+}
---
+++ p2b.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	float base,height,area;
+	
+	printf("triangle");
+	scanf("%f",&base);
+	
+	printf("enter");
+	scanf("%f",&height);
+	
+	area=(base*height)/2;
+	
+}
